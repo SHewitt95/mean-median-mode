5fb399296d59fe6712db0408a5800b090a1bd1b0
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-02-28 11:54:19 -0500

    Added return statements to getNumber() and mode because they are important

--- mean_median_mode.py
+++ mean_median_mode.py
@@ -7,7 +7,7 @@ def main():
     length_of_list = getNumber("Enter the amount of numbers you want to use: ")
     
     index = 1 # Acts as counter so user knows how many numbers have been entered so far
-    while (len(numbers) != length_of_list):
+    for num in range(0, length_of_list):
         item = getNumber("Enter number %d: " %(index))
         numbers.append(item)
         index += 1
@@ -19,20 +19,23 @@ def main():
     print("")
     
     print("Mean: " + mean())
-    print("Median: " + median())
-    print("Mode: " + mode())
+    print("Median: " + str(median()))
+    print("Mode: (mode, # of times mode appeared)" + mode())
     
 def getNumber(prompt):
     # Prompts user for starting number. Exception/While loop 
     # catches inputs that aren't proper integers.
     success = False
+    number = 0
     while (not success):
         try:
             print(prompt)
-            number = raw_input()
+            number = int(raw_input())
             success = True
         except:
             pass
+        
+    return number
     
 def mean():
     return str((sum(numbers)/len(numbers)))
@@ -52,49 +55,8 @@ def mode():
     data = Counter(numbers)
     #data.most_common()
     mode = str(data.most_common(1))
+    
+    return mode
 
 if (__name__ == "__main__"):
     main()
-
-'''
-#Fix so it can loop if user inputs string for "max_number"
-while (max_number <= 0):
-    max_number = int(raw_input("Enter the amount of numbers you want to use: "))
-    
-index = 1    
-while (len(numbers) != max_number):
-    try:
-        item = float(raw_input("Enter a number (%d): " %index))
-        numbers.append(item)
-        index += 1
-    except ValueError:
-        item = float(raw_input("Please enter a numeral: "))
-        numbers.append(item)
-        index += 1
-
-print ""
-print "Here are your numbers: " + str(numbers)
-numbers.sort()
-print "Here are your numbers, sorted: " + str(numbers)
-print ""
-
-### Mean ###
-print "Mean: " + str((sum(numbers)/len(numbers)))
-
-### Median ###
-if ((len(numbers)%2) != 0):
-    median = numbers[len(numbers)/2]
-else:
-    digit_first = numbers[len(numbers)/2 - 1]
-    digit_last = numbers[len(numbers)/2]
-    median = (digit_first + digit_last)/2.0    
-print "Median: " + str(median)
-
-### Mode ###
-data = Counter(numbers)
-data.most_common()
-mode = str(data.most_common(1))
-print "Mode (mode, # of times mode appeared): " + mode
-print ""
-raw_input("Press Enter when finished")
-'''
